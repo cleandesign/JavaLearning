8ea8b990323d5ad502285e958214ac43f17a033c
kangzhidong <kzd666@163.com>
2018-07-02 20:43:01 +0800

    Merge branch 'master' of https://github.com/KangZhiDong/JavaLearning
    # Conflicts:
    #	README.md

--- src/com/woshidaniu/Java_Interview/Java_Interview_crossoverJie/src/main/java/com/crossoverjie/Application.java
+++ src/com/woshidaniu/Java_Interview/Java_Interview_crossoverJie/src/main/java/com/crossoverjie/Application.java
@@ -1,24 +1,24 @@
-package com.woshidaniu.Java_Interview.Java_Interview.src.main.java.com.crossoverjie;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-/**
- * @author crossoverJie
- *
- */
-@SpringBootApplication
-public class Application {
-
-    private final static Logger LOGGER = LoggerFactory.getLogger(Application.class);
-
-
-    public static void main(String[] args) throws Exception {
-        SpringApplication.run(Application.class, args);
-        LOGGER.info("start ok!");
-
-    }
-
-}
+//package com.woshidaniu.Java_Interview.Java_Interview.src.main.java.com.crossoverjie;
+//
+//import org.slf4j.Logger;
+//import org.slf4j.LoggerFactory;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+///**
+// * @author crossoverJie
+// *
+// */
+//@SpringBootApplication
+//public class Application {
+//
+//    private final static Logger LOGGER = LoggerFactory.getLogger(Application.class);
+//
+//
+//    public static void main(String[] args) throws Exception {
+//        SpringApplication.run(Application.class, args);
+//        LOGGER.info("start ok!");
+//
+//    }
+//
+//}
